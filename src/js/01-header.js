f3b8aa620be63f69fd80c6d97262b404044c902c
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-04-11 11:16:13 +0300

    Scroll

--- src/js/01-header.js
+++ src/js/01-header.js
@@ -15,15 +15,15 @@ gsap.registerPlugin(
   ScrollToPlugin
 );
 
-const burger = document.querySelector('.header__burger__menu');
+const burger = document.querySelector('.header__burger-menu');
 const mobileMenu = document.querySelector('.header__modal');
 const body = document.body;
 
 let scrollPosition = 0;
 
 const openMenu = () => {
-  burger.classList.add('is-open');
-  mobileMenu.classList.add('is-open');
+  burger.classList.add('header__burger-menu--open');
+  mobileMenu.classList.add('header__modal--open');
 
   scrollPosition = window.scrollY;
 
@@ -44,48 +44,58 @@ const openMenu = () => {
 };
 
 const closeMenu = () => {
-  burger.classList.remove('is-open');
-  mobileMenu.classList.remove('is-open');
+  burger.classList.remove('header__burger-menu--open');
+  mobileMenu.classList.remove('header__modal--open');
   body.classList.remove('menu-open');
   body.style.top = '';
-  window.scrollTo(0, scrollPosition);
 
   gsap.to(mobileMenu, {
     x: '100%',
     duration: 0.4,
     ease: 'power2.in',
     autoAlpha: 0,
+    onComplete: () => {
+      window.scrollTo({ top: scrollPosition, behavior: 'instant' });
+    },
   });
 };
 
 burger.addEventListener('click', () => {
-  const isOpen = burger.classList.contains('is-open');
+  const isOpen = burger.classList.contains('header__burger-menu--open');
   isOpen ? closeMenu() : openMenu();
 });
 
-document.querySelectorAll('.header__modal__item a').forEach(link => {
-  link.classList.add('scroll-link');
-});
+const sectionOffsets = {
+  home: 1,
+  whispers: 1,
+  gardens: 1,
+  moments: 1,
+  plant: 1,
+};
 
-document.querySelectorAll('.scroll-link').forEach(link => {
+document.querySelectorAll('[data-target]').forEach(link => {
   link.addEventListener('click', event => {
     event.preventDefault();
-    const targetId = link.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
+    const targetSection = link.getAttribute('data-target');
+    const targetElement = document.querySelector(
+      `[data-section="${targetSection}"]`
+    );
 
     if (targetElement) {
       closeMenu();
       const headerOffset =
-        document.querySelector('.header__box').offsetHeight || 109;
+        document.querySelector('.header__box')?.offsetHeight || 109;
+      const offsetAdjustment = sectionOffsets[targetSection] || 20;
       const y =
         targetElement.getBoundingClientRect().top +
         window.pageYOffset -
-        headerOffset;
+        headerOffset -
+        offsetAdjustment;
 
       gsap.to(window, {
-        scrollTo: { y: y, autoKill: true },
-        duration: 1,
-        ease: 'sine.inOut',
+        scrollTo: { y: y, autoKill: false },
+        duration: 1.2,
+        ease: 'power3.out',
       });
     }
   });
