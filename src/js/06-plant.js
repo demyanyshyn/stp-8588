5ff3de7ffdf663beba21ee5c2a240fcbecee9b90
demyanyshyn <taras.demyanyshyn@gmail.com>
2025-04-15 14:02:23 +0300

    clear comments

--- src/js/06-plant.js
+++ src/js/06-plant.js
@@ -1,13 +1,5 @@
 import Swiper from 'swiper';
 
-import {
-  Autoplay,
-  EffectCards,
-  EffectCoverflow,
-  Navigation,
-  Pagination,
-} from 'swiper/modules';
-
 const swiperOptions = {
   slidesPerView: 5,
   spaceBetween: 16,
@@ -43,41 +35,3 @@ const swiperOptions = {
 };
 ////
 const swiper = new Swiper('.swiper', swiperOptions);
-
-// import Swiper from 'swiper';
-// import { EffectCards } from 'swiper/modules';
-// import 'swiper/css/effect-cards';
-
-// const swiperParams = {
-//   effect: 'cards',
-//   grabCursor: true,
-//   cardsEffect: {
-//     perSlideOffset: 8,
-//     perSlideRotate: 2,
-//     slideShadows: true,
-//   },
-//   loop: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// };
-//   {autoplay: {
-//     delay: 1500,
-//   },
-//   direction: 'horizontal',
-//   centeredSlides: true,
-//   effect: 'cards',
-//   cardsEffect: {
-//     perSlideOffset: 5,
-//     perSlideRotate: 20,
-//     rotate: true,
-//     slideShadows: true,
-//   },
-//   grid: { colums: 5 },
-//   loop: true,
-//   pagination: true,
-//   setWrapperSize: true,
-// };
-
-// const swiper = new Swiper('.swiper', swiperParams);
